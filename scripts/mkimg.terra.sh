de83040b2149cae59c0636957e4dba4c041eba02
Evan Hazlett <ejhazlett@gmail.com>
2019-11-04 17:17:08 -0500

    add terraos-installer; update custom iso generation
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- scripts/mkimg.terra.sh
+++ scripts/mkimg.terra.sh
@@ -1,12 +1,14 @@
-profile_zfs() {
+profile_terra() {
         profile_standard
         kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
         syslinux_serial="0 115200"
         kernel_addons="zfs spl"
-        apks="$apks iscsi-scst efibootmgr vim zfs-scripts zfs zfs-utils-py
-                cciss_vol_status lvm2 mdadm mkinitfs mtools nfs-utils
+	apkovl="genapkovl-terra.sh"
+	hostname="terra"
+        apks="$apks iscsi-scst efibootmgr bash dialog vim zfs-scripts zfs zfs-utils-py
+                cciss_vol_status lvm2 mdadm mkinitfs mtools 
                 parted rsync sfdisk syslinux unrar util-linux xfsprogs
-                dosfstools ntfs-3g cryptsetup cfdisk
+                dosfstools ntfs-3g terraos-installer
                 "
         local _k _a
         for _k in $kernel_flavors; do
